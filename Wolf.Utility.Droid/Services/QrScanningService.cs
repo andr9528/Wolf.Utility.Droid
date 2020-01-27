5dc44fd2e2e5cfbaa9bb8db6f106216a1a760c8f
André Steenhoff Madsen <andre@steenhoff.dk>
2020-01-27 14:24:32 +0100

    end of day
    Created my own WebView, so that i could try and get an event when it was touched, but didn't work.
    
    ALso created a Qr scanning service as that is gonna be needed to scan qr codes at some point in the future

---
+++ Wolf.Utility.Droid/Services/QrScanningService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Wolf.Utility.Droid.Services;
+using Wolf.Utility.Main.Xamarin;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+[assembly: Dependency(typeof(QrScanningService))]
+namespace Wolf.Utility.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync(MobileBarcodeScanningOptions options)
+        {
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Scan the QR Code",
+                BottomText = "Please Wait" 
+            };
+
+            var scanResult = await scanner.Scan(options);
+            return scanResult.Text;
+        }
+    }
+}
