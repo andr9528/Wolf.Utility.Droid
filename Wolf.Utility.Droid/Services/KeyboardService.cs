054f05c35fd6854423b6313efaaf67519ce84dfb
André Steenhoff Madsen <andre@steenhoff.dk>
2020-02-24 15:03:20 +0100

    end of day
    attempted to fix keyboard not closing on android 9, with no success yet - asked for assistance on stackoverflow: https://stackoverflow.com/questions/60377493/unable-to-hide-android-keyboard-on-android-9

--- Wolf.Utility.Droid/Services/KeyboardService.cs
+++ Wolf.Utility.Droid/Services/KeyboardService.cs
@@ -1,9 +1,11 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Views;
 using Android.Views.InputMethods;
 using Wolf.Utility.Droid.Services;
+using Wolf.Utility.Main.Logging;
+using Wolf.Utility.Main.Logging.Enum;
 using Wolf.Utility.Main.Xamarin.Services;
-using Xamarin.Forms;
 
 namespace Wolf.Utility.Droid.Services
 {
@@ -20,14 +22,53 @@ namespace Wolf.Utility.Droid.Services
 
         public void HideKeyboard()
         {
-            if (inputMethodManager != null && mainActivity is Activity)
-            {
-                var activity = mainActivity as Activity;
-                var token = activity?.CurrentFocus?.WindowToken;
-                inputMethodManager.HideSoftInputFromWindow(token, HideSoftInputFlags.None);
+            if (inputMethodManager == null || !(mainActivity is Activity activity)) return;
 
-                activity?.Window.DecorView.ClearFocus();
+            Logging.Log(LogType.Information, $"Attempting to Hide Keyboard via 1st method...");
+
+            //var view = activity.CurrentFocus;
+            var view = activity.FindViewById(Android.Resource.Id.Content).RootView;
+            if (view == null) Logging.Log(LogType.Warning, $"Failed to get View from Activity...");
+
+            var token = view?.WindowToken;
+            if (token == null) Logging.Log(LogType.Warning, $"Failed to get Token from View...");
+
+            var success = inputMethodManager.HideSoftInputFromWindow(token, HideSoftInputFlags.None);
+            Logging.Log(LogType.Information,
+                $"{nameof(inputMethodManager.HideSoftInputFromWindow)} returned => {success}");
+            
+            if(success) view?.ClearFocus();
+            if (!IsKeyboardShown)
+            {
+                view?.ClearFocus();
+                return;
             }
+
+            Logging.Log(LogType.Warning,
+                $"Failed to Hide Keyboard via {nameof(inputMethodManager.HideSoftInputFromWindow)}...");
+            HideKeyboardAttemptTwo(activity);
+        }
+        
+        private void HideKeyboardAttemptTwo(Activity activity)
+        {
+            Logging.Log(LogType.Information, $"Attempting to Hide Keyboard via 2nd method...");
+
+            //var view = activity.CurrentFocus;
+            var view = activity.FindViewById(Android.Resource.Id.Content).RootView;
+            if (view == null) Logging.Log(LogType.Warning, $"Failed to get View from Activity...");
+
+            var token = view?.WindowToken;
+            if (token == null) Logging.Log(LogType.Warning, $"Failed to get Token from View...");
+
+            inputMethodManager.ToggleSoftInputFromWindow(token, ShowSoftInputFlags.None, HideSoftInputFlags.None);
+            
+            if (!IsKeyboardShown)
+            {
+                view?.ClearFocus();
+                return;
+            }
+
+            Logging.Log(LogType.Warning, $"Failed to Hide Keyboard via {nameof(inputMethodManager.ToggleSoftInputFromWindow)}...");
         }
 
         public void ReInitializeInputMethod()
