82d21e867206003bd31a690c0a92c964b6a7b242
André Steenhoff Madsen <andre@steenhoff.dk>
2020-05-11 18:23:07 +0200

    Added OnStartCommand to SignalRService class, which simple tries to reconnect when resuming the app, if the connection and settings tells it to do so.

--- Wolf.Utility.Droid/Services/SignalRService.cs
+++ Wolf.Utility.Droid/Services/SignalRService.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Microsoft.AspNetCore.SignalR.Client;
 using Wolf.Utility.Main.SignalR;
 
 namespace Wolf.Utility.Droid.Services
@@ -17,5 +18,15 @@ namespace Wolf.Utility.Droid.Services
     {
         protected readonly string SignalRChannelId = $"{nameof(T)} Notifications";
         public T HubProxy { get; protected set; }
+        protected bool ReconnectOnResume { get; set; }
+        protected int ReconnectAttempts { get; set; }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            if(ReconnectOnResume && HubProxy.ConnectionState == HubConnectionState.Disconnected) 
+                HubProxy.Reconnect(ReconnectAttempts);
+
+            return StartCommandResult.Sticky;
+        }
     }
 }   
